9ad8c985f6d5df4278d648869803c3dd14b10d25
fr3chtoa45t <jcronier777@gmail.com>
2021-11-03 19:10:15 -0700

    Add error flag when an interviewer is not selected in the form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -23,6 +23,11 @@ export default function Form(props) {
       return;
     }
 
+    if (!interviewer) {
+      setError("Please choose an interviewer");
+      return;
+    }
+
     setError("");
     props.onSave(student, interviewer);
   }
